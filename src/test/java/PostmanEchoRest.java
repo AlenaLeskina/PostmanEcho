07e5a367fd51a04b2a3ff5018a4bc0c281d4e152
AlenaLeskina <andryushhenko-1994@mail.ru>
2022-02-19 20:06:44 +0300

    project postmanEcho

---
+++ src/test/java/PostmanEchoRest.java
@@ -0,0 +1,27 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanEchoRest {
+    @Test
+    void shouldReturnPostRequest() {
+        //Подход Given-When-Then
+        //Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test")
+                //Выполняемые действия
+                .when()
+                .post("/post")
+                //Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test"));
+
+    }
+}
+
+
